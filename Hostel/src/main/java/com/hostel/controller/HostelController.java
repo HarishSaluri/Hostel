bc93a28abd4674777ba77b351172ae980f2fdc67
harish <saluri7001@gmail.com>
2018-06-18 01:03:57 +0530

    Initial commit of Hostel Application with hello service

---
+++ Hostel/src/main/java/com/hostel/controller/HostelController.java
@@ -0,0 +1,15 @@
+package com.hostel.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/rest")
+public class HostelController {
+	
+	@RequestMapping("/")
+	public String getHello() {
+		return "HELLO";
+	}
+
+}
