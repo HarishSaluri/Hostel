aa75a4338affc62b9238841d14370c8133bfde26
harish <saluri7001@gmail.com>
2018-06-21 00:02:08 +0530

    Second Commit

--- Hostel/src/main/java/com/hostel/controller/HostelController.java
+++ Hostel/src/main/java/com/hostel/controller/HostelController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/rest")
 public class HostelController {
 	
-	@RequestMapping("/")
+	@RequestMapping("")
 	public String getHello() {
 		return "HELLO";
 	}
